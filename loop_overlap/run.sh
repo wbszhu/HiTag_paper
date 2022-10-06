f86df7c1fb1ce99aa782709c16ad14a3485f99e2
luzhang <48794258+wbszhu@users.noreply.github.com>
2022-10-06 11:22:39 +0800

    Create run.sh

---
+++ loop_overlap/run.sh
@@ -0,0 +1,9 @@
+bash 00.overlap.sh \
+ HiTag_Loop HiTag_1DpeakLoop \
+ /data2/zhanglu/tmp/hitag/fig/loop_compare/use_cuttagcommad/00.rawdata  \
+ /data2/zhanglu/tmp/hitag/fig/loop_compare/use_cuttagcommad/out
+
+bash 01.get_normalize.sh HiTag_Loop \
+ HiTag_1DpeakLoop \
+ /data2/zhanglu/tmp/hitag/fig/loop_compare/use_cuttagcommad/00.rawdata  \
+ /data2/zhanglu/tmp/hitag/fig/loop_compare/use_cuttagcommad/out
